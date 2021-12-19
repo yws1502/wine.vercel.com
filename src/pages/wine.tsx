4b8bb20eaf6cc11c6af324a3cbfa1a567febf892
Woosang <yws1502@naver.com>
2021-12-19 20:54:34 +0900

    init commit

---
+++ src/pages/wine.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+// Q. import 가 뭔가요? > 몰라도 됌
+
+const WinePage: NextPage = () => {
+  return (
+    <div>
+      <h1>Wine</h1>
+    </div>
+  )
+}
+
+export default WinePage;
+// Q. export 가 뭔가요 > 몰라도 됌
+// 가장 기본적인 템플릿, TypeScript를 쓰는
