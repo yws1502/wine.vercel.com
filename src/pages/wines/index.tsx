54deeadc32f821c1c06cae3845270871d9c8b576
Woosang <yws1502@naver.com>
2021-12-21 19:03:12 +0900

    create index.tsx

---
+++ src/pages/wines/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+// Q. import 가 뭔가요? > 몰라도 됌
+
+const WinePage: NextPage = () => {
+  return (
+    <div>
+      <h1>Wine</h1>
+    </div>
+  )
+}
+
+export default WinePage;
